dde5fa456a178844bdd2e3a72af18a508bb03559
Antonio <antonio@Antonios-MacBook-Pro.local>
2022-11-05 20:38:36 -0400

    First Commit :)

---
+++ Rest Application/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Rest Application
+//
+//  Created by Antonio on 10/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textlabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textlabel.text = "Hi "
+    }
+
+
+}
+
