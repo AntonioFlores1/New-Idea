4dfc16e10d57a803f9ce80e0d1250cc0e9fc9376
Antonio <antonio@Antonios-MacBook-Pro.local>
2022-11-19 22:33:47 -0500

    Set up UISearchBar & UINavigation with UINav title

--- Rest Application/MainUserScreen/MainUserRestViewController.swift
+++ Rest Application/MainUserScreen/MainUserRestViewController.swift
@@ -9,19 +9,49 @@ import UIKit
 
 class MainUserRestViewController: UIViewController {
     
+    var mainUserView = MainUserView()
     
     @IBOutlet weak var textlabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.navigationController?.navigationBar.prefersLargeTitles = true
+//        self.navigationController?.navigationBar.preferredBehavioralStyle =
+//        self.navigationItem.setTitle(<#T##title: String##String#>, subtitle: T##String)
+        self.navigationItem.largeTitleDisplayMode = .never
+        self.navigationItem.largeTitleDisplayMode = .always
         
+        view.addSubview(mainUserView)
+//        self.navigationItem.titleView?.backgroundColor = .red
+//        self.navigationItem.setTitle("Hello Antonio Good morning", subtitle: "Back up Test")
+        self.navigationItem.title = "Hello Antonio Good morning"
+
+//        self.navigationItem.prompt = "Back up Test"
         textlabel.text = "Hi"
         
-        
     }
-
     
     
 
 }
 
+extension UINavigationItem {
+
+    func setTitle(_ title: String, subtitle: String) {
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.font = .systemFont(ofSize: 20.0)
+        titleLabel.textColor = .black
+
+        let subtitleLabel = UILabel()
+        subtitleLabel.text = subtitle
+        subtitleLabel.font = .systemFont(ofSize: 17.0)
+        subtitleLabel.textColor = .darkGray
+
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
+        stackView.alignment = .leading
+        stackView.axis = .vertical
+        self.titleView = stackView
+    }
+
+}
--- Rest Application/MainUserScreen/MainUserView.swift
+++ Rest Application/MainUserScreen/MainUserView.swift
@@ -8,13 +8,41 @@
 import UIKit
 
 class MainUserView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+   
+    public lazy var mainUserSearchBar: UISearchBar = {
+        let mainUserSearchBar = UISearchBar()
+        mainUserSearchBar.backgroundColor = .blue
+           return mainUserSearchBar
+       }()
+    
+    
+        
+    override init(frame: CGRect) {
+            super.init(frame: UIScreen.main.bounds)
+            commonInit()
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            commonInit()
+        }
+    
+    private func commonInit(){
+        mainUserUIConstraints()
+        }
+    
+    private func mainUserUIConstraints(){
+        mainUserSearchBarConstraints()
+       }
+    
+    
+    private func mainUserSearchBarConstraints(){
+            addSubview(mainUserSearchBar)
+        mainUserSearchBar.translatesAutoresizingMaskIntoConstraints = false
+        mainUserSearchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        mainUserSearchBar.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        mainUserSearchBar.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        }
+    
+    
 }
